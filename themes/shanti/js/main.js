4e89f8972a4b03a7fe8a36fd76e4903376f57f1f
artemtyrsin <artemtyrsin@yandex.ru>
2018-06-12 19:28:07 +0500

    public theme shanti

---
+++ themes/shanti/js/main.js
@@ -0,0 +1,22 @@
+$(function() {
+
+  if ($.fn.slick) {
+    $(".slider").slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true,
+      arrows: false,
+      draggable: true,
+      fade: true
+    });
+  }
+
+  if($.fn.gpGallery) {
+    $(".photos").gpGallery('.photo__item', {
+      'row_min_height':280,
+      'row_max_height':360,
+      'gutter':6
+    });
+  }
+});
